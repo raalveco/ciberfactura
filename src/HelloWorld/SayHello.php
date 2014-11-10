cc980c82ae3679f27bcb8bdb772e676dad71625f
Ramiro Vera <raalveco@gmail.com>
2014-11-09 23:51:14 -0600

    Inicializando el Paquete de Composer

---
+++ src/HelloWorld/SayHello.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * User: raalveco
+ * Date: 09/11/2014
+ * Time: 11:24 PM
+ */
+
+namespace HelloWorld;
+
+class SayHello
+{
+    public static function world()
+    {
+        return 'Hello World, Composer!';
+    }
+}
