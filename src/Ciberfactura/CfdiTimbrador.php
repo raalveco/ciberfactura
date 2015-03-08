d21b5fc7b5e580127cef0723142fc02d147b7644
Ramiro Vera <raalveco@gmail.com>
2015-03-07 20:08:33 -0600

    Exceptions en el Timbrador

--- src/Ciberfactura/CfdiTimbrador.php
+++ src/Ciberfactura/CfdiTimbrador.php
@@ -22,13 +22,13 @@
         public function __construct(){
             try{
                 if(Config::get('packages/raalveco/ciberfactura/config.production')){
-                    $url_autentificar = Config::get('packages/raalveco/ciberfactura/pac.url_autentificar');
-                    $url_timbrar = Config::get('packages/raalveco/ciberfactura/pac.url_timbrar');
-                    $url_cancelar = Config::get('packages/raalveco/ciberfactura/pac.url_cancelar');
+                    $url_autentificar = Config::get('packages/raalveco/ciberfactura/config.pac.url_autentificar');
+                    $url_timbrar = Config::get('packages/raalveco/ciberfactura/config.pac.url_timbrar');
+                    $url_cancelar = Config::get('packages/raalveco/ciberfactura/config.pac.url_cancelar');
 
-                    $this->usuario = Config::get('packages/raalveco/ciberfactura/pac.usuario');
-                    $this->password = Config::get('packages/raalveco/ciberfactura/pac.password');
-                    $this->rfc = Config::get('packages/raalveco/ciberfactura/pac.rfc');
+                    $this->usuario = Config::get('packages/raalveco/ciberfactura/config.pac.usuario');
+                    $this->password = Config::get('packages/raalveco/ciberfactura/config.pac.password');
+                    $this->rfc = Config::get('packages/raalveco/ciberfactura/config.rfc');
 
                     $url_cer = app_path()."/config/packages/raalveco/ciberfactura/certificados/".Config::get('packages/raalveco/ciberfactura/config.cer');
                     $url_key = app_path()."/config/packages/raalveco/ciberfactura/certificados/".Config::get('packages/raalveco/ciberfactura/config.key');
@@ -36,17 +36,17 @@
 
                 }
                 else{
-                    $url_autentificar = "https://pruebascfdi.smartweb.com.mx/Autenticacion/wsAutenticacion.asmx?wsdl";
-                    $url_timbrar = "https://pruebascfdi.smartweb.com.mx/Timbrado/wsTimbrado.asmx?wsdl";
-                    $url_cancelar = "https://pruebascfdi.smartweb.com.mx/Autenticacion/wsAutenticacion.asmx?wsdl";
+                    $url_autentificar = Config::get('packages/raalveco/ciberfactura/config.pac_test.url_autentificar');
+                    $url_timbrar = Config::get('packages/raalveco/ciberfactura/config.pac_test.url_timbrar');
+                    $url_cancelar = Config::get('packages/raalveco/ciberfactura/config.pac_test.url_cancelar');
 
-                    $this->usuario = "demo";
-                    $this->password = "123456789";
-                    $this->rfc = 'AAD990814BP7';
+                    $this->usuario = Config::get('packages/raalveco/ciberfactura/config.pac_test.usuario');
+                    $this->password = Config::get('packages/raalveco/ciberfactura/config.pac_test.password');
+                    $this->rfc = Config::get('packages/raalveco/ciberfactura/config.rfc');
 
-                    $url_cer = app_path()."/config/packages/raalveco/ciberfactura/certificados/test/aad990814bp7_1210261233s.cer";
-                    $url_key = app_path()."/config/packages/raalveco/ciberfactura/certificados/test/aad990814bp7_1210261233s.key";
-                    $clave_privada = "12345678a";
+                    $url_cer = app_path()."/config/packages/raalveco/ciberfactura/certificados/".Config::get('packages/raalveco/ciberfactura/config.cer');
+                    $url_key = app_path()."/config/packages/raalveco/ciberfactura/certificados/".Config::get('packages/raalveco/ciberfactura/config.key');
+                    $clave_privada = Config::get('packages/raalveco/ciberfactura/config.clave_privada');
                 }
 
                 $this->soap_autentificar = new \SoapClient($url_autentificar, array("trace" => 1, "exception" => 0));
--- src/config/config.php
+++ src/config/config.php
@@ -11,10 +11,25 @@
  */
 
 return array(
-    'production' => false,
-	'cer' => '00001000000302642746.cer',
+    'production' => true,
+    'rfc' => 'VECR8307073J1',
+    'cer' => '00001000000302642746.cer',
     'key' => '00001000000302642746.key',
     'clave_privada' => 'Ramiro07',
+    'pac' => array(
+        'url_autentificar' => 'https://cfdi.smartweb.com.mx/Autenticacion/wsAutenticacion.asmx?wsdl',
+        'url_timbrar' => 'https://cfdi.smartweb.com.mx/Timbrado/wsTimbrado.asmx?wsdl',
+        'url_cancelar' => 'https://cfdi.smartweb.com.mx/Cancelacion/wsCancelacion.asmx?wsdl',
+        'usuario' => 'demo',        //Cambiarán cuando se contrate el Servicio de Timbres
+        'password' => '123456789',  //Cambiarán cuando se contrate el Servicio de Timbres
+    ),
+    'pac_test' => array(
+        'url_autentificar' => 'https://pruebascfdi.smartweb.com.mx/Autenticacion/wsAutenticacion.asmx?wsdl',
+        'url_timbrar' => 'https://pruebascfdi.smartweb.com.mx/Timbrado/wsTimbrado.asmx?wsdl',
+        'url_cancelar' => 'https://pruebascfdi.smartweb.com.mx/Cancelacion/wsCancelacion.asmx?wsdl',
+        'usuario' => 'demo',
+        'password' => '123456789',
+    ),
     'path_xmls' => public_path().'/cfdis',
-    'logo' => 'logotipo.png'
+    'logo' => 'logotipo.png',
 );
